6575782caa2cb76116a1856e7ccc9bd94b056153
johnhasgone <eu.vaganov@gmail.com>
2021-10-09 13:14:03 +0300

    start homework 5

---
+++ spring-05-homework/src/main/java/ru/otus/spring05homework/Spring05HomeworkApplication.java
@@ -0,0 +1,13 @@
+package ru.otus.spring05homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring05HomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring05HomeworkApplication.class, args);
+    }
+
+}
---
+++ spring-05-homework/src/test/java/ru/otus/spring05homework/Spring05HomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.spring05homework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring05HomeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
