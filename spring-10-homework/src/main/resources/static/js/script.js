e6175d58ea629ed6db3be535309e3bad4b6111f7
johnhasgone <eu.vaganov@gmail.com>
2021-12-06 23:42:19 +0300

    homework_10 wip

--- spring-10-homework/src/main/resources/static/js/script.js
+++ spring-10-homework/src/main/resources/static/js/script.js
@@ -4,7 +4,7 @@ const bookEditForm =
     `<form id="edit-form" action = "/rest/book/save">
             <div class="book-info">
                 
-                <div class="row">
+                <div class="row" id="row-id">
                     <label class="book-label" for="id-input"><b>ID:</b></label>
                     <input class="book-edit-input" id="id-input" readonly name="id" type="text"/>
                 </div>
@@ -25,7 +25,7 @@ const bookEditForm =
                 </div>
             </div>
             <div>
-                <button class="edit-button" type="submit">Сохранить</button>
+                <button class="page-button" type="submit">Сохранить</button>
             </div>
     </form>`;
 
@@ -47,14 +47,133 @@ function getStartPage() {
     newContent.append(listDiv);
 
     header.innerHTML = "Библиотека";
-    document.getElementsByClassName("container")[0].replaceChild(newContent, content);
+    setNewContent(newContent);
 }
 
 function getCreatePage() {
+    const newContent = createNewContentWithBackButton(getStartPage);
 
-    header.innerHTML = "Ввести информацию о книге";
-    document.getElementById('content').innerHTML = bookEditForm;
+    const formDiv = document.createElement('div');
+    formDiv.innerHTML = bookEditForm;
+    newContent.append(formDiv);
+
+    header.innerText = "Ввести информацию о книге";
+    setNewContent(newContent);
+    document.getElementById('row-id').remove();
     document.getElementById('edit-form').addEventListener("submit", handleSaveBookSubmit);
+}
+
+function createNewContentWithBackButton(action) {
+    const newContent = document.createElement('div');
+    newContent.id = 'content';
+
+    const backDiv = document.createElement('div');
+    backDiv.className = 'back-button';
+    const backButton = createButton('page-button', 'back-button', null, 'Назад', action);
+    backDiv.append(backButton);
+    newContent.append(backDiv);
+
+    return newContent;
+}
+
+async function getBookPage(event) {
+    const newContent = createNewContentWithBackButton(getAllBooks);
+    const tableDiv = document.createElement('div');
+    const commentDiv = document.createElement('div');
+
+    newContent.append(tableDiv);
+
+    await fetch('/rest/book/' + event.target.value)
+        .then(response => response.json())
+        .then(book => {
+            tableDiv.innerHTML =
+                `<table class="book-info">
+                            <tr class="row">
+                                <td class="title"><b>ID:</b></td>
+                                <td>${book.id}</td>
+                            </tr>
+                            <tr class="row">
+                                <td class="title"><b>Название:</b></td>
+                                <td class="value">${book.title}</td>
+                            </tr>
+                            <tr class="row">
+                                <td class="title"><b>Автор(ы):</b></td>
+                                <td class="value">${book.authors}</td>
+                            </tr>
+                            <tr class="row">
+                                <td class="title"><b>Жанр(ы):</b></td>
+                                <td class="value">${book.genres}</td>
+                            </tr>
+                        </table>`;
+        });
+
+    header.innerHTML = `Информация о книге`;
+    setNewContent(newContent);
+}
+
+async function getEditPage(event) {
+    const newContent = createNewContentWithBackButton(getAllBooks);
+
+    const formDiv = document.createElement('div');
+    formDiv.innerHTML = bookEditForm;
+    newContent.append(formDiv);
+
+    header.innerHTML = `Редактировать информацию о книге`;
+    setNewContent(newContent);
+    await getBookInfo(event);
+    document.getElementById('edit-form').addEventListener("submit", handleSaveBookSubmit);
+}
+
+async function getAllBooks() {
+    const newContent = document.createElement('div');
+    newContent.id = 'content';
+
+    const backDiv = document.createElement('div');
+    backDiv.className = 'back-button';
+    const backButton = createButton('page-button', 'back-button', null,'Назад', getStartPage);
+    backDiv.append(backButton);
+    newContent.append(backDiv);
+
+    const table = document.createElement('table');
+    table.className = 'books';
+    const head = document.createElement('thead');
+    head.innerHTML =
+        `<tr>
+            <th>ID</th>
+            <th>Название</th>
+            <th>Автор(ы)</th>
+            <th>Жанр(ы)</th>
+        </tr>`;
+    const tbody = document.createElement('tbody');
+
+    table.append(head);
+
+    await fetch('/rest/book')
+        .then(response => response.json())
+        .then(books => books.forEach(function (book) {
+            const tr = document.createElement('tr');
+
+            const id = document.createElement('td');
+            id.innerHTML = book.id;
+            const title = document.createElement('td');
+            title.innerHTML = book.title;
+            const authors = document.createElement('td');
+            authors.innerHTML = book.authors;
+            const genres = document.createElement('td');
+            genres.innerHTML = book.genres;
+
+            const openButton = createButton('action-button', 'open-button', book.id, 'Открыть', getBookPage);
+            const editButton = createButton('action-button', 'edit-button', book.id, 'Изменить', getEditPage);
+            const deleteButton = createButton('action-button', 'delete-button', book.id, 'Удалить', deleteBook);
+
+            tr.append(id, title, authors, genres, openButton, editButton, deleteButton);
+            tbody.append(tr);
+        }));
+    table.appendChild(tbody);
+    newContent.append(table);
+
+    header.innerText = 'Список книг';
+    setNewContent(newContent);
 }
 
 async function handleSaveBookSubmit(event) {
@@ -96,59 +215,6 @@ async function saveBook({ url, formData }) {
     return response.json();
 }
 
-function getAllBooks() {
-    const oldContent = document.getElementById('content');
-    const newContent = document.createElement('div');
-    newContent.id = 'content';
-
-    const backDiv = document.createElement('div');
-    backDiv.className = 'back-button';
-    const backButton = createButton('page-button', 'back-button', null,'Назад', getStartPage);
-    backDiv.append(backButton);
-    newContent.append(backDiv);
-
-    const table = document.createElement('table');
-    table.className = 'books';
-    const head = document.createElement('thead');
-    head.innerHTML =
-        `<tr>
-            <th>ID</th>
-            <th>Название</th>
-            <th>Автор(ы)</th>
-            <th>Жанр(ы)</th>
-        </tr>`;
-    const tbody = document.createElement('tbody');
-
-    table.append(head);
-
-    fetch('/rest/book')
-        .then(response => response.json())
-        .then(books => books.forEach(function (book) {
-            const tr = document.createElement('tr');
-
-            const id = document.createElement('td');
-            id.innerHTML = book.id;
-            const title = document.createElement('td');
-            title.innerHTML = book.title;
-            const authors = document.createElement('td');
-            authors.innerHTML = book.authors;
-            const genres = document.createElement('td');
-            genres.innerHTML = book.genres;
-
-            const openButton = createButton('action-button', 'open-button', book.id, 'Открыть', getBookPage);
-            const editButton = createButton('action-button', 'edit-button', book.id, 'Изменить', editBook);
-            const deleteButton = createButton('action-button', 'delete-button', book.id, 'Удалить', deleteBook);
-
-            tr.append(id, title, authors, genres, openButton, editButton, deleteButton);
-            tbody.append(tr);
-        }));
-    table.appendChild(tbody);
-    newContent.append(table);
-
-    header.innerText = 'Список книг';
-    document.getElementsByClassName('container')[0].replaceChild(newContent, oldContent);
-}
-
 function createButton(className, id, value, innerText, eventOption) {
     const button = document.createElement('button');
     button.className = className;
@@ -159,68 +225,23 @@ function createButton(className, id, value, innerText, eventOption) {
     return button;
 }
 
-async function getBookPage(event) {
-    const oldContent = document.getElementById('content');
-    const newContent = document.createElement('div');
-    newContent.id = 'content';
-
-    const backDiv = document.createElement('div');
-    backDiv.className = 'back-button';
-    const backButton = document.createElement('button');
-    backButton.className = "page-button";
-    backButton.id = "back-button";
-    backButton.innerText = "Назад";
-    backButton.addEventListener('click', getAllBooks);
-    backDiv.append(backButton);
-    newContent.append(backDiv);
-
-    const tableDiv = document.createElement('div');
-    newContent.append(tableDiv);
-
-    await fetch('/rest/book/' + event.target.value)
-        .then(response => response.json())
-        .then(book => {
-            header.innerHTML = `Информация о книге`;
-            tableDiv.innerHTML =
-                `<table class="book-info">
-                            <tr class="row">
-                                <td class="title"><b>ID:</b></td>
-                                <td>${book.id}</td>
-                            </tr>
-                            <tr class="row">
-                                <td class="title"><b>Название:</b></td>
-                                <td class="value">${book.title}</td>
-                            </tr>
-                            <tr class="row">
-                                <td class="title"><b>Автор(ы):</b></td>
-                                <td class="value">${book.authors}</td>
-                            </tr>
-                            <tr class="row">
-                                <td class="title"><b>Жанр(ы):</b></td>
-                                <td class="value">${book.genres}</td>
-                            </tr>
-                        </table>`;
-        });
-    document.getElementsByClassName('container')[0].replaceChild(newContent, oldContent);
-}
-
 async function deleteBook(event) {
     await fetch('/rest/book/' + event.target.value, {method: 'DELETE'});
     getAllBooks();
 }
 
-async function editBook(event) {
+async function getBookInfo(event) {
     await fetch('/rest/book/' + event.target.value)
         .then(response => response.json())
         .then(book => {
-            header.innerHTML = `Редактировать информацию о книге`;
-            document.getElementById('content').innerHTML = bookEditForm;
-
             document.getElementById('id-input').value = book.id;
             document.getElementById('title-input').value = book.title;
             document.getElementById('author-input').value = book.authors;
             document.getElementById('genre-input').value = book.genres;
-
-            document.getElementById('edit-form').addEventListener("submit", handleSaveBookSubmit);
         });
+}
+
+function setNewContent(newContent) {
+    document.getElementById('container')
+        .replaceChild(newContent, document.getElementById('content'));
 }
