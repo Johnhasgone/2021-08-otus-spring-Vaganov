c4ab5b6951acf4db9beba9039e5fbaa1ef36c26b
johnhasgone <89quAdrA89>
2022-01-16 22:39:14 +0300

    homework 15 started

---
+++ spring-15-homework/src/main/java/ru/otus/spring15homework/Spring15HomeworkApplication.java
@@ -0,0 +1,13 @@
+package ru.otus.spring15homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring15HomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring15HomeworkApplication.class, args);
+    }
+
+}
